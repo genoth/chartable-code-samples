c76eea2a1a13b1640c3396cf5e3d6e96cb652beb
Gwynne Noth <nothgwynne@gmail.com>
2017-09-28 21:39:50 -0500

    Create basic pie chart without labels

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+//2.4 Creating a simple bar chart
+
+var bardata = [20, 30, 45, 15];
+var height = 400,
+    width = 600,
+    barWidth = 50,
+    barOffset = 5;
+
+var yScale = d3.scaleLinear()
+
+d3.select('#viz').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .style('background', '#C9D7D6')
+.selectAll('rect').data(bardata)
+  .enter().append('rect')
+    .style('fill', '#C61C6F')
+    .attr('width', barWidth)
+    .attr('height', function(d) {
+      return d;
+    })
+    .attr('x', function(d, i) {
+      return i*(barWidth + barOffset)
+    })
+    .attr('y', function(d) {
+      return height - d;
+    });
+
+//2.3 Drawing SVG graphics with D3
+// d3.select('#viz')
+//   .append('svg')
+//     .attr('width', 600)
+//     .attr('height', 400)
+//     .style('background', '#93A1A1')
+//   .append('rect')
+//     .attr('x', 200)
+//     .attr('y', 100)
+//     .attr('height', 200)
+//     .attr('width', 200)
+//     .style('fill', '#CB4B19');
+
+// d3.select('#viz svg')
+//   .append('circle')
+//   .attr('cx', 300)
+//   .attr('cy', 200)
+//   .attr('r', 50)
+//   .style('fill', 'blue')
+
+
+
 // d3.selectAll("tr:nth-child(even) .day-high .temp").text("hot");
 
 // d3.selectAll("tr:nth-child(even) .day-high .temp")
@@ -62,21 +112,21 @@
 
 
 // this doesn't work. not sure why
-var mydata = [
-  { date: '4/01/2017', low: 50, high: 78 },
-  { date: '4/02/2017', low: 55, high: 79 },
-  { date: '4/03/2017', low: 40, high: 68 },
-  { date: '4/04/2017', low: 50, high: 74 },
-  { date: '4/05/2017', low: 67, high: 92 },
-];
+// var mydata = [
+//   { date: '4/01/2017', low: 50, high: 78 },
+//   { date: '4/02/2017', low: 55, high: 79 },
+//   { date: '4/03/2017', low: 40, high: 68 },
+//   { date: '4/04/2017', low: 50, high: 74 },
+//   { date: '4/05/2017', low: 67, high: 92 },
+// ];
 
-d3.select('tbody')
-  .selectAll('tr')
-  .data(mydata)
-  .enter().append('tr')
-  .html(function(d) {
-    return '<th scope="row">' + d.date + '</th><td>' + d.low + '</td><td>' + d.high + '</td>'
-  })
+// d3.select('tbody')
+//   .selectAll('tr')
+//   .data(mydata)
+//   .enter().append('tr')
+//   .html(function(d) {
+//     return '<th scope="row">' + d.date + '</th><td>' + d.low + '</td><td>' + d.high + '</td>'
+//   })
 
 
 
